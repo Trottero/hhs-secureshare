a84f19a1ac6c76586368ef61d0f25cf5b4de503a
Sava Sharif <savaaa_@hotmail.com>
2018-03-20 23:46:19 +0100

    Added popper.js lib. Fixed redirecting after signing in. Renamed a partial. Improved upload screen and front page.

--- SecureShare/Controllers/AccountController.cs
+++ SecureShare/Controllers/AccountController.cs
@@ -5,44 +5,40 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SecureShare.Website.Controllers
 {
-    [Route("[controller]/[action]")]
-    public class AccountController : Controller
-    {
-        [HttpGet]
-        public IActionResult SignIn()
-        {
-            var redirectUrl = Url.Action(nameof(HomeController.Index), "Home");
-            return Challenge(
-                new AuthenticationProperties { RedirectUri = redirectUrl },
-                OpenIdConnectDefaults.AuthenticationScheme);
-        }
+	[Route("[controller]/[action]")]
+	public class AccountController : Controller
+	{
+		[HttpGet]
+		public IActionResult SignIn()
+		{
+			var redirectUrl = Url.Action("Dashboard", "PrivateUser");
+			return Challenge(
+				new AuthenticationProperties {RedirectUri = redirectUrl},
+				OpenIdConnectDefaults.AuthenticationScheme);
+		}
 
-        [HttpGet]
-        public IActionResult SignOut()
-        {
-            var callbackUrl = Url.Action(nameof(SignedOut), "Account", values: null, protocol: Request.Scheme);
-            return SignOut(
-                new AuthenticationProperties { RedirectUri = callbackUrl },
-                CookieAuthenticationDefaults.AuthenticationScheme,
-                OpenIdConnectDefaults.AuthenticationScheme);
-        }
+		[HttpGet]
+		public IActionResult SignOut()
+		{
+			var callbackUrl = Url.Action(nameof(SignedOut), "Account", values: null, protocol: Request.Scheme);
+			return SignOut(
+				new AuthenticationProperties {RedirectUri = callbackUrl},
+				CookieAuthenticationDefaults.AuthenticationScheme,
+				OpenIdConnectDefaults.AuthenticationScheme);
+		}
 
-        [HttpGet]
-        public IActionResult SignedOut()
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-                // Redirect to home page if the user is authenticated.
-                return RedirectToAction(nameof(HomeController.Index), "Home");
-            }
+		[HttpGet]
+		public IActionResult SignedOut()
+		{
+			if (User.Identity.IsAuthenticated) return RedirectToAction("Dashboard", "PrivateUser");
 
-            return View();
-        }
+			return RedirectToAction(nameof(HomeController.Index), "Home");
+		}
 
-        [HttpGet]
-        public IActionResult AccessDenied()
-        {
-            return View();
-        }
-    }
+		[HttpGet]
+		public IActionResult AccessDenied()
+		{
+			return View();
+		}
+	}
 }
