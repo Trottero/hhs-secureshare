db155b562142715701e18e163e3c3082d97d3100
Niels Witte <15080250@student.hhs.nl>
2018-04-06 15:08:34 +0200

    Added home controller tests

---
+++ SecureShare.Test/HomeControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecureShare.Webapp.Controllers;
+using Xunit;
+
+namespace SecureShare.Test
+{
+    public class HomeControllerTest : ControllerBase
+    {
+        private readonly HomeController _controllerUnderTest;
+
+
+        public HomeControllerTest()
+        {
+            _controllerUnderTest = new HomeController();
+        }
+
+
+        [Fact]
+        public void Returns_Index_View()
+        {
+            // Act
+            var result = _controllerUnderTest.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Same(result, viewResult);
+        }
+
+        [Fact]
+        public void Returns_Pricing_View()
+        {
+            // Act
+            var result = _controllerUnderTest.Pricing();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Same(result, viewResult);
+        }
+
+        [Fact]
+        public void Returns_About_View()
+        {
+            // Act
+            var result = _controllerUnderTest.About();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Same(result, viewResult);
+        }
+    }
+}
